6327a568a711d3f6c3d1906ac4f7b3aa242284a8
Yuta Saito <kateinoigakukun@gmail.com>
2020-05-27 11:12:26 +0900

    wip

--- Sources/CodeGen/CodeGen.swift
+++ Sources/CodeGen/CodeGen.swift
@@ -1,6 +1,36 @@
 import LLVM
 import Parser
 
-func irgen(program: MainProgram) {
+public func irgen(program: MainProgram) {
+    let igm = IRGenModule(moduleName: program.programStmt.name)
+}
 
+
+class IRGenModule {
+    
+    let builder: IRBuilder
+    let module: Module
+    
+    init(moduleName: String) {
+        module = Module(name: moduleName)
+        builder = IRBuilder(module: module)
+    }
+    
+    
+    func emitMainProgram(program: MainProgram) {
+        let main = builder.addFunction("main", type: FunctionType([], IntType.int64))
+        let entry = main.appendBasicBlock(named: "entry")
+        builder.positionAtEnd(of: entry)
+        
+        for execution in program.executions {
+            emitExecutableConstruct(executable: execution)
+        }
+    }
+    
+    func emitExecutableConstruct(executable: ExecutableConstruct) {
+    }
+    
+    func emitWriteStmt(stmt: WriteStmt) {
+        
+    }
 }
